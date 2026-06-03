3fc55591041735a2ab6d25ac7e1f6162eeed5f6f
Muhammed Shareef <shareefnncr@gmail.com>
2026-06-03 11:56:47 +0530

    Fix: Add Cache-Control and Pragma to CORS allowedHeaders

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma"],
   optionsSuccessStatus: 200
 };
 
